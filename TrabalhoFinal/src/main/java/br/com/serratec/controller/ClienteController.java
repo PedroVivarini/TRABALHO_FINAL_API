152606232142727c06182e795a52594983e9d80a
CumuloWinter <cumulowinter@gmail.com>
2025-10-29 15:05:37 -0300

    feat: documentação de categoria, cliente e pedido adicionadas

--- TrabalhoFinal/src/main/java/br/com/serratec/controller/ClienteController.java
+++ TrabalhoFinal/src/main/java/br/com/serratec/controller/ClienteController.java
@@ -9,7 +9,13 @@ import java.util.UUID;
 
 import br.com.serratec.dto.ClienteRequestDTO;
 import br.com.serratec.dto.ClienteResponseDTO;
+import br.com.serratec.entity.Cliente;
 import br.com.serratec.service.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -21,11 +27,20 @@ public class ClienteController {
     @Autowired
     private ClienteService clienteService; 
 
+        @Operation(summary = "Listagem de Clientes", description = "Lista todos os peditos do sistema em um arquivo JSON")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", content = {
+		@Content(schema = @Schema(implementation = Cliente.class),mediaType = "application/json") }, description = "Retorna a listagem de clientes"),
+		@ApiResponse(responseCode = "401",description = "Erro de Autenticação"),
+		@ApiResponse(responseCode = "403",description = "Não há permissão para acessar o recurso"),
+		@ApiResponse(responseCode = "404",description = "Recurso não encontrado"),
+		@ApiResponse(responseCode = "505",description = "Exceção interna da aplicação"),
+	})
     @GetMapping
     public ResponseEntity<List<ClienteResponseDTO>> listar() {
         return ResponseEntity.ok(clienteService.listar());
     }
- 
+    
+    @Operation(summary = "Inserir Cliente", description = "Insere um cliente")
     @PostMapping
     public ResponseEntity<ClienteResponseDTO> inserir(
             @Valid @RequestBody ClienteRequestDTO dto
@@ -34,6 +49,7 @@ public class ClienteController {
         return ResponseEntity.status(HttpStatus.CREATED).body(clienteSalvo);
     }
 
+    @Operation(summary = "Editar Cliente", description = "Edita um cliente")
     @PutMapping("/{id}")
     public ResponseEntity<ClienteResponseDTO> editar(
             @PathVariable UUID id,           
--- TrabalhoFinal/src/main/java/br/com/serratec/controller/PedidoController.java
+++ TrabalhoFinal/src/main/java/br/com/serratec/controller/PedidoController.java
@@ -16,8 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 
 import br.com.serratec.dto.PedidoRequestDTO;
 import br.com.serratec.dto.PedidoResponseDTO;
+import br.com.serratec.entity.Pedido;
 import br.com.serratec.enums.StatusPedido;
 import br.com.serratec.service.PedidoService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 
@@ -32,18 +38,28 @@ public class PedidoController {
         this.pedidoService = pedidoService;
     }
 
+    @Operation(summary = "Listagem de pedidos", description = "Lista todos os pedidos do sistema em um arquivo JSON")
+	@ApiResponses(value = {@ApiResponse(responseCode = "200", content = {
+		@Content(schema = @Schema(implementation = Pedido.class),mediaType = "application/json") }, description = "Retorna a listagem de pedidos"),
+		@ApiResponse(responseCode = "401",description = "Erro de Autenticação"),
+		@ApiResponse(responseCode = "403",description = "Não há permissão para acessar o recurso"),
+		@ApiResponse(responseCode = "404",description = "Recurso não encontrado"),
+		@ApiResponse(responseCode = "505",description = "Exceção interna da aplicação"),
+	})
     @GetMapping
     public ResponseEntity<List<PedidoResponseDTO>> listar() {
         List<PedidoResponseDTO> response = pedidoService.listar();
         return ResponseEntity.ok(response);
     }
     
+    @Operation(summary = "Inserir pedido", description = "Insere um pedido")
     @PostMapping
     public ResponseEntity<PedidoResponseDTO> inserir(@Valid @RequestBody PedidoRequestDTO dto) {
         PedidoResponseDTO response = pedidoService.inserir(dto);
         return ResponseEntity.status(HttpStatus.CREATED).body(response);
     }
 
+    @Operation(summary = "Editar pedido", description = "Edita um pedido")
     @PutMapping("/{id}")
     public ResponseEntity<PedidoResponseDTO> editar(
             @PathVariable UUID id,
